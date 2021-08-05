4737c824e2d91fa08ec1a5ae159fad832055b23d
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-08-05 03:47:25 -0300

    Consulta CEP

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,28 +1,65 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const fs = require('fs');
+const { readFile, writeFile } = require('fs').promises;
 
 app.use(express.json());
 
 app.get('/enderecos/:cep', async (req, res) => {
     const cep = req.params.cep;
 
-    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-    const conteudo = {
-        "cep": response.data.cep,
-        "logradouro": response.data.logradouro,
-        "complemento": response.data.complemento,
-        "bairro": response.data.bairro,
-        "localidade": response.data.localidade,
-        "uf": response.data.uf,
-        "ibge": response.data.ibge,
-        "gia": response.data.gia,
-        "ddd": response.data.ddd,
-        "siafi": response.data.siafi
-    };
+    async function novoCep(nomeArquivo, conteudo) {
+        const data = await readFile(nomeArquivo, 'utf-8');
+        const json = JSON.parse(data || '[]');
 
-    res.json(conteudo);
+        if (!Array.isArray(json)) {
+            throw new Error(`Malformed JSON. Expected array, got: ${json}.`);
+        }
+        json.push(conteudo);
+
+        const jsonString = JSON.stringify(json);
+
+        await writeFile(nomeArquivo, jsonString);
+    }
+
+    async function verificar(nomeArquivo) {
+        const data = await readFile(nomeArquivo, 'utf-8');
+        const json = JSON.parse(data || '[]');
+        const cepFormatado = cep.substr(0, 5) + "-" + cep.substr(-3);
+        let index = -1;
+
+        for (i = 0; i < json.length; i++) {
+            if (json[i].cep === cepFormatado) {
+                index = i;
+            }
+        }
+
+        if (index !== -1) {
+            res.json(json[index]);
+            return;
+        } else {
+            const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+            const conteudo = {
+                "cep": response.data.cep,
+                "logradouro": response.data.logradouro,
+                "complemento": response.data.complemento,
+                "bairro": response.data.bairro,
+                "localidade": response.data.localidade,
+                "uf": response.data.uf,
+                "ibge": response.data.ibge,
+                "gia": response.data.gia,
+                "ddd": response.data.ddd,
+                "siafi": response.data.siafi
+            };
+
+            novoCep('enderecos.json', conteudo)
+                .catch(console.error);
+            res.json(conteudo);
+            return;
+        }
+    }
+
+    verificar('enderecos.json');
 });
 
 app.listen(8000);
